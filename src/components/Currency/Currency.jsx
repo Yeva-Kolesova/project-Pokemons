ad76a9ae4b25263b20c6ab6b85018fc1e6d480e6
Kolesova Yeva <ykolsw@gmail.com>
2024-01-11 11:11:16 +0200

    currency fix path

---
+++ src/components/Currency/Currency.jsx
@@ -0,0 +1,11 @@
+export const Currency = () => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Currency</th>
+                </tr>  
+            </thead>
+        </table>
+    );
+} 
