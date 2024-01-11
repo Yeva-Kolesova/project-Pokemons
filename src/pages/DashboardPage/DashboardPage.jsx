43c027832efc8e0bc7be930e6be8847744c70ff2
SVTroya <my.svtroy@gmail.com>
2024-01-11 21:51:35 +0200

    Gradients

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -4,17 +4,25 @@ import Navigation from '../../components/Navigation/Navigation';
 import { Outlet } from 'react-router-dom';
 import Balance from '../../components/Balance/Balance';
 import Container from '../../components/Container/Container';
-import { DashboardContainer, LeftContainer, NavAndBalanceWrapper, SectionContainer } from './DashboardPage.styled';
+import {
+  DashboardContainer, GradientLeftBottom, GradientLeftTop,
+  GradientRight,
+  LeftContainer,
+  NavAndBalanceWrapper,
+} from './DashboardPage.styled';
 import { useMediaQuery } from 'react-responsive';
 import { CurrencyPage } from '../CurrencyPage';
 
 function DashboardPage() {
   const isTabletOrDesktop = useMediaQuery({ query: '(min-width: 768px)' });
   return (
-    <SectionContainer>
+    <>
       <Header />
       <Container>
         <DashboardContainer>
+          <GradientRight/>
+          <GradientLeftBottom/>
+          <GradientLeftTop/>
           <LeftContainer>
             <NavAndBalanceWrapper>
               <Navigation />
@@ -27,7 +35,7 @@ function DashboardPage() {
           </Suspense>
         </DashboardContainer>
       </Container>
-    </SectionContainer>
+    </>
   );
 }
 
--- src/pages/DashboardPage/DashboardPage.styled.jsx
+++ src/pages/DashboardPage/DashboardPage.styled.jsx
@@ -1,25 +1,14 @@
 import styled from 'styled-components';
-import bcgDesktopPath from '../../images/desctopStatistic.webp'
-import bcgTabletPath from '../../images/tabletStatistic.webp'
-import bcgMobPath from '../../images/mobileHome.webp'
 
 export const SectionContainer = styled.section`
-  background-image: url(${bcgMobPath});
 
-  @media only screen and (min-width: 768px) {
-    background-image: url(${bcgTabletPath});
-  }
-
-  @media only screen and (min-width: 1280px) {
-    background-image: url(${bcgDesktopPath});
-    flex-direction: row;
-  }
 `;
 
 export const DashboardContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
+  position: relative;
 
   @media only screen and (min-width: 768px) {
     gap: 20px;
@@ -27,7 +16,41 @@ export const DashboardContainer = styled.div`
 
   @media only screen and (min-width: 1280px) {
     flex-direction: row;
+    height: calc(100vh - 80px);
   }
+`;
+
+export const GradientRight = styled.div`
+  width: 893px;
+  height: 901px;
+  border-radius: 901px;
+  background: linear-gradient(220deg, rgba(109, 84, 235, 0.60) 28.31%, rgba(101, 35, 146, 0.60) 66.76%);
+  filter: blur(200px);
+  position: absolute;
+  top: 197px;
+  left: 682px;
+`;
+
+export const GradientLeftBottom = styled.div`
+  width: 760px;
+  height: 766px;
+  border-radius: 766px;
+  background: linear-gradient(207deg, #341FA0 46.11%, #652392 63.65%);
+  filter: blur(200px);
+  position: absolute;
+  top: 189px;
+  right: 700px;
+`;
+
+export const GradientLeftTop = styled.div`
+  width: 739px;
+  height: 745px;
+  border-radius: 745px;
+  background: linear-gradient(220deg, rgba(109, 84, 235, 0.60) 28.31%, rgba(101, 35, 146, 0.60) 66.76%);
+  filter: blur(200px);
+  position: absolute;
+  top: -244px;
+  right: 748px;
 `;
 
 export const NavAndBalanceWrapper = styled.div`
