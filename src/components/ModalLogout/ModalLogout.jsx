127c2bfab5beb7b0d98e54145da6d0d3ca86f3d7
kkot04 <144848298+kkot04@users.noreply.github.com>
2024-01-12 11:43:34 +0200

    startLogOut

---
+++ src/components/ModalLogout/ModalLogout.jsx
@@ -0,0 +1,34 @@
+import React, {useEffect} from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logOutThunk } from "reduxConfig/auth/operations";
+
+
+const ModalLogout = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const logOut = () => {
+    dispatch(logOutThunk())
+    .then(() => {
+        navigate('/')
+    })
+    .catch(error)
+  }
+
+
+  return (
+    <div>
+        {/* <svg>
+            <use/>
+        </svg> */}
+        <h3>Money Guard</h3>
+        <p>Are you sure you want to log out?</p>
+        <button onClick={logOut}>logout</button>
+        <button>cancel</button>
+    </div>
+  )
+}
+
+export default ModalLogout
+
