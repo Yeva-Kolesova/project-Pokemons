1731c5be9c131daa27d28ea84591ccc7fda59c9b
Kolesova Yeva <ykolsw@gmail.com>
2024-01-13 09:39:09 +0200

    currency adding

--- src/reduxConfig/currency/slice.js
+++ src/reduxConfig/currency/slice.js
@@ -1,22 +1,27 @@
-// import { createSlice } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
 import { fetchCurrency } from './operations';
 
 const initialState = {
-    currency: {
-        usdRate: {
-            buy: null,
-            sell: null,
-        },
-        euroRate: {
-            buy: null,
-            sell: null,
-        },
-    },
-    time: '',
+    data: [],
     isLoading: false,
     error: null,
 };
+
+function handlePending(state) {
+    state.isLoading = true;
+    state.error = null;
+}
+
+function handleFulfilled(state, action) {
+    state.data = action.payload;
+    state.isLoading = false;
+    state.error = null;
+}
+
+function handleRejected(state, { error }) {
+    state.isLoading = false;
+    state.error = error;
+}
 
 const currencySlice = createSlice({
     name: 'currency',
@@ -24,18 +29,9 @@ const currencySlice = createSlice({
     reducers: {},
     extraReducers: (builder) =>
         builder
-            .addCase(fetchCurrency.fulfilled, (state, { payload }) => {
-                state.currency = payload;
-                state.isLoading = false;
-                state.error = null;
-            })
-            .addCase(fetchCurrency.pending, (state) => {
-                state.isLoading = true;
-            })
-            .addCase(fetchCurrency.rejected, (state, { payload }) => {
-                state.isLoading = false;
-                state.error = payload;
-            }),
+            .addCase(fetchCurrency.fulfilled, handleFulfilled)
+            .addCase(fetchCurrency.pending, handlePending)
+            .addCase(fetchCurrency.rejected, handleRejected),
 });
 
 export const currencyReducer = currencySlice.reducer;
