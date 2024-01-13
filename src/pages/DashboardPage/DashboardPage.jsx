4eeabdded59f88775e94443a033d2b9ff55bee45
SVTroya <my.svtroy@gmail.com>
2024-01-13 19:12:07 +0200

    Fixed db methods for transactions

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
 import Header from '../../components/Header/Header';
 import Navigation from '../../components/Navigation/Navigation';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Balance from '../../components/Balance/Balance';
 import Container from '../../components/Container/Container';
 import {
@@ -15,6 +15,9 @@ import  CurrencyPage  from '../CurrencyPage';
 
 function DashboardPage() {
   const isTabletOrDesktop = useMediaQuery({ query: '(min-width: 768px)' });
+  const location = useLocation();
+  const currentPage = location.pathname.replace('dashboard', '').replaceAll('/', '');
+
   return (
     <SectionContainer>
       <Header />
@@ -27,7 +30,8 @@ function DashboardPage() {
           <LeftContainer>
             <NavAndBalanceWrapper>
               <Navigation />
-              <Balance />
+              {!isTabletOrDesktop && (currentPage === 'home' || currentPage === '') && <Balance />}
+              {isTabletOrDesktop && <Balance />}
             </NavAndBalanceWrapper>
             {isTabletOrDesktop && <CurrencyPage />}
           </LeftContainer>
