c7857a1229bca03c4376a3a58146adc950598911
MariiaZhk <maria8zhuk@gmail.com>
2024-01-13 13:08:04 +0100

    add-transactions-yup

--- src/components/AddTransactionModal/Calendar/Calendar.jsx
+++ src/components/AddTransactionModal/Calendar/Calendar.jsx
@@ -1,30 +1,20 @@
 import { useState } from 'react';
 import ReactDatePicker from 'react-datepicker';
-// import sprite from '../sprite.svg';
-import { CalendarS, DateWrapper } from './Calendar.styled';
+import { CalendarIcon, CalendarS, DateWrapper } from './Calendar.styled';
 
 export const Calendar = () => {
   const [startDate, setStartDate] = useState(new Date());
   return (
     <DateWrapper>
-      {/* <>
-        {
-          <svg>
-            <use
-              xlinkHref={`${sprite}#icon-date_range`}
-              width={24}
-              height={24}
-            />
-          </svg>
-        }
-      </> */}
       <ReactDatePicker
+        name="date"
         selected={startDate}
         onChange={date => setStartDate(date)}
         dateFormat="dd.MM.yyyy"
         maxDate={new Date()}
         calendarContainer={CalendarS}
       />
+      <CalendarIcon />
     </DateWrapper>
   );
 };
