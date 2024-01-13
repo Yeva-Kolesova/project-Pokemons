64242c84995d542d69963a029f45b978599ea330
MariiaZhk <maria8zhuk@gmail.com>
2024-01-13 13:33:49 +0100

    fix+styles

--- src/components/AddTransactionModal/AddTransaction/AddTransaction.jsx
+++ src/components/AddTransactionModal/AddTransaction/AddTransaction.jsx
@@ -1,31 +1,30 @@
-
-­import { selectCategories } from 'reduxConfig/transactions/selectors';
+import { selectCategories } from 'reduxConfig/transactions/selectors';
 import { CloseBtn } from './AddTransaction.styled';
 import {
-ActiveExpense,
-ActiveIncome,
-Backdrop,
-BtnAdd,
-BtnCancel,
-ButtonsWrap,
-CloseModalBtn,
-Gradient,
-Form,
-Input,
-Modal,
-NoActive,
-TextareaStyled,
-Title,
-TransactionToggleWrap,
-WrapSumCalendar,
+  ActiveExpense,
+  ActiveIncome,
+  Backdrop,
+  BtnAdd,
+  BtnCancel,
+  ButtonsWrap,
+  CloseModalBtn,
+  Gradient,
+  Form,
+  Input,
+  Modal,
+  NoActive,
+  TextareaStyled,
+  Title,
+  TransactionToggleWrap,
+  WrapSumCalendar,
 } from './AddTransaction.styled';
 import 'react-datepicker/dist/react-datepicker.css';
 import React, { useEffect, useState } from 'react';
 import { InputToggle, LabelToggle, SpanToggle } from './AddTransaction.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import {
-addTransactionThunk,
-getTransactionsCategoriesThunk,
+  addTransactionThunk,
+  getTransactionsCategoriesThunk,
 } from '../../../reduxConfig/transactions/operations';
 import Select, { components } from 'react-select';
 import { SlArrowDown, SlArrowUp } from 'react-icons/sl';
@@ -35,241 +34,241 @@ import { useForm } from 'react-hook-form';
 import { Calendar } from '../Calendar/Calendar';
 
 const validationSchema = yup
-.object({
-amount: yup
-.number()
-.typeError('Transaction value must be a number')
-.min(1, 'Sum value must be at least 1 character')
-.required('Sum is required'),
-date: yup.date().required('Please provide transaction date.'),
-comment: yup
-.string()
-.min(3, 'Comment must be at least 3 characters')
-.max(30)
-.required('Comment is required'),
-})
-.required();
+  .object({
+    amount: yup
+      .number()
+      .typeError('Transaction value must be a number')
+      .min(1, 'Sum value must be at least 1 character')
+      .required('Sum is required'),
+    date: yup.date().required('Please provide transaction date.'),
+    comment: yup
+      .string()
+      .min(3, 'Comment must be at least 3 characters')
+      .max(30)
+      .required('Comment is required'),
+  })
+  .required();
 
 export const AddTransaction = ({ closeModal }) => {
-const [isMinus, setIsMinus] = useState(true);
-const [startDate] = useState(new Date());
-const dispatch = useDispatch();
-const transactionCategories = useSelector(selectCategories);
+  const [isMinus, setIsMinus] = useState(true);
+  const [startDate] = useState(new Date());
+  const dispatch = useDispatch();
+  const transactionCategories = useSelector(selectCategories);
 
-const {
-register,
-handleSubmit,
-// formState: { errors },
-} = useForm({
-mode: 'onChange',
-resolver: yupResolver(validationSchema),
-});
+  const {
+    register,
+    handleSubmit,
+    // formState: { errors },
+  } = useForm({
+    mode: 'onChange',
+    resolver: yupResolver(validationSchema),
+  });
 
-useEffect(() => {
-dispatch(getTransactionsCategoriesThunk());
-}, [dispatch]);
+  useEffect(() => {
+    dispatch(getTransactionsCategoriesThunk());
+  }, [dispatch]);
 
-useEffect(() => {
-document.body.style.overflow = 'hidden';
-return () => {
-document.body.style.overflow = 'auto';
-};
-}, []);
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
 
-const onBackdropClick = e => {
-if (e.target === e.currentTarget) {
-closeModal();
-}
-};
+  const onBackdropClick = e => {
+    if (e.target === e.currentTarget) {
+      closeModal();
+    }
+  };
 
-useEffect(() => {
-const handleEscape = e => {
-if (e.key === 'Escape') {
-closeModal();
-}
-};
+  useEffect(() => {
+    const handleEscape = e => {
+      if (e.key === 'Escape') {
+        closeModal();
+      }
+    };
 
-document.addEventListener('keydown', handleEscape);
-return () => {
-document.removeEventListener('keydown', handleEscape);
-};
-}, [closeModal]);
+    document.addEventListener('keydown', handleEscape);
+    return () => {
+      document.removeEventListener('keydown', handleEscape);
+    };
+  }, [closeModal]);
 
-const onCancelButton = () => {
-closeModal();
-};
+  const onCancelButton = () => {
+    closeModal();
+  };
 
-const transformDate = function getFormattedDate() {
-const year = startDate.getFullYear();
-const month = String(startDate.getMonth() + 1).padStart(2, '0');
-const day = String(startDate.getDate()).padStart(2, '0');
-const hours = String(startDate.getHours()).padStart(2, '0');
-const minutes = String(startDate.getMinutes()).padStart(2, '0');
-const seconds = String(startDate.getSeconds()).padStart(2, '0');
-const transformedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-return transformedDate;
-};
+  const transformDate = function getFormattedDate() {
+    const year = startDate.getFullYear();
+    const month = String(startDate.getMonth() + 1).padStart(2, '0');
+    const day = String(startDate.getDate()).padStart(2, '0');
+    const hours = String(startDate.getHours()).padStart(2, '0');
+    const minutes = String(startDate.getMinutes()).padStart(2, '0');
+    const seconds = String(startDate.getSeconds()).padStart(2, '0');
+    const transformedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    return transformedDate;
+  };
 
-const submit = e => {
-const transaction = {
-transactionDate: transformDate(startDate),
-};
-dispatch(addTransactionThunk(transaction))
-.unwrap()
-.then(() => {
-console.log('Transaction added');
-})
-.catch(err => {
-console.log(err);
-});
-};
+  const submit = e => {
+    const transaction = {
+      transactionDate: transformDate(startDate),
+    };
+    dispatch(addTransactionThunk(transaction))
+      .unwrap()
+      .then(() => {
+        console.log('Transaction added');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-const selectStyle = {
-control: styles => ({
-...styles,
-backgroundColor: 'transparent',
-marginBottom: '-2px',
-marginTop: '40px',
-border: 'none',
-borderBottom: '1px solid rgba(255, 255, 255, 0.4)',
-outline: 'none',
-borderRadius: '0',
-boxShadow: 'none',
-'&:hover': {
-border: 'none',
-borderBottom: '1px solid rgba(255, 255, 255, 0.4)',
-},
-}),
-singleValue: styles => ({
-...styles,
-color: '#FBFBFB',
-fontSize: '18px',
-}),
-placeholder: styles => ({
-...styles,
-color: 'rgba(255, 255, 255, 0.6)',
-fontSize: '18px',
-}),
-menu: styles => ({
-...styles,
-borderRadius: '8px',
-backgroundColor: 'transparent',
-boxShadow: '0px 4px 60px 0px rgba(0, 0, 0, 0.25)',
-backdropFilter: 'blur(50px)',
-overflow: 'hidden',
-color: '#FBFBFB',
-fontFamily: "'Poppins', sans-serif",
-fontSize: '16px',
-fontWeight: '400',
+  const selectStyle = {
+    control: styles => ({
+      ...styles,
+      backgroundColor: 'transparent',
+      marginBottom: '-2px',
+      marginTop: '40px',
+      border: 'none',
+      borderBottom: '1px solid rgba(255, 255, 255, 0.4)',
+      outline: 'none',
+      borderRadius: '0',
+      boxShadow: 'none',
+      '&:hover': {
+        border: 'none',
+        borderBottom: '1px solid rgba(255, 255, 255, 0.4)',
+      },
+    }),
+    singleValue: styles => ({
+      ...styles,
+      color: '#FBFBFB',
+      fontSize: '18px',
+    }),
+    placeholder: styles => ({
+      ...styles,
+      color: 'rgba(255, 255, 255, 0.6)',
+      fontSize: '18px',
+    }),
+    menu: styles => ({
+      ...styles,
+      borderRadius: '8px',
+      backgroundColor: 'transparent',
+      boxShadow: '0px 4px 60px 0px rgba(0, 0, 0, 0.25)',
+      backdropFilter: 'blur(50px)',
+      overflow: 'hidden',
+      color: '#FBFBFB',
+      fontFamily: "'Poppins', sans-serif",
+      fontSize: '16px',
+      fontWeight: '400',
 
-'&::before': {
-background:
-'linear-gradient(0deg, rgba(83, 61, 186, 0.70) 0%, rgba(80, 48, 154, 0.70) 43.14%, rgba(106, 70, 165, 0.52) 73.27%, rgba(133, 93, 175, 0.13) 120.03%)',
-content: '""',
-filter: 'blur(50px)',
-position: 'absolute',
-inset: '0%',
-zIndex: '-1',
-},
-}),
-option: (styles, { isFocused, isSelected }) => {
-if (isFocused) {
-return {
-...styles,
-background: '#FFFFFF1A',
-color: '#FF868D',
-};
-} else if (isSelected) {
-return {
-...styles,
-background: 'transparent',
-};
-} else {
-return {
-...styles,
-};
-}
-},
-};
+      '&::before': {
+        background:
+          'linear-gradient(0deg, rgba(83, 61, 186, 0.70) 0%, rgba(80, 48, 154, 0.70) 43.14%, rgba(106, 70, 165, 0.52) 73.27%, rgba(133, 93, 175, 0.13) 120.03%)',
+        content: '""',
+        filter: 'blur(50px)',
+        position: 'absolute',
+        inset: '0%',
+        zIndex: '-1',
+      },
+    }),
+    option: (styles, { isFocused, isSelected }) => {
+      if (isFocused) {
+        return {
+          ...styles,
+          background: '#FFFFFF1A',
+          color: '#FF868D',
+        };
+      } else if (isSelected) {
+        return {
+          ...styles,
+          background: 'transparent',
+        };
+      } else {
+        return {
+          ...styles,
+        };
+      }
+    },
+  };
 
-const DropdownIndicator = props => {
-return (
-<components.DropdownIndicator {...props}>
-{props.selectProps.menuIsOpen ? (
-<SlArrowUp size={18} label="Arrow down" color={'var(--white)'} />
-) : (
-<SlArrowDown size={18} label="Arrow down" color={'var(--white)'} />
-)}
-</components.DropdownIndicator>
-);
-};
+  const DropdownIndicator = props => {
+    return (
+      <components.DropdownIndicator {...props}>
+        {props.selectProps.menuIsOpen ? (
+          <SlArrowUp size={18} label="Arrow down" color={'var(--white)'} />
+        ) : (
+          <SlArrowDown size={18} label="Arrow down" color={'var(--white)'} />
+        )}
+      </components.DropdownIndicator>
+    );
+  };
 
-return (
-<Backdrop onClick={onBackdropClick}>
-<Modal>
-<Gradient />
-<CloseModalBtn type="button" onClick={() => closeModal()}>
-<CloseBtn />
-</CloseModalBtn>
-<Title>Add transaction</Title>
-<Form onSubmit={handleSubmit(submit)}>
-<TransactionToggleWrap>
-{isMinus ? (
-<NoActive>Income</NoActive>
-) : (
-<ActiveIncome>Income</ActiveIncome>
-)}
-<LabelToggle>
-<InputToggle
-type="checkbox"
-defaultChecked
-onChange={() => setIsMinus(!isMinus)}
-/>
-<SpanToggle />
-</LabelToggle>
+  return (
+    <Backdrop onClick={onBackdropClick}>
+      <Modal>
+        <Gradient />
+        <CloseModalBtn type="button" onClick={() => closeModal()}>
+          <CloseBtn />
+        </CloseModalBtn>
+        <Title>Add transaction</Title>
+        <Form onSubmit={handleSubmit(submit)}>
+          <TransactionToggleWrap>
+            {isMinus ? (
+              <NoActive>Income</NoActive>
+            ) : (
+              <ActiveIncome>Income</ActiveIncome>
+            )}
+            <LabelToggle>
+              <InputToggle
+                type="checkbox"
+                defaultChecked
+                onChange={() => setIsMinus(!isMinus)}
+              />
+              <SpanToggle />
+            </LabelToggle>
 
-{isMinus ? (
-<ActiveExpense>Expense</ActiveExpense>
-) : (
-<NoActive>Expense</NoActive>
-)}
-</TransactionToggleWrap>
+            {isMinus ? (
+              <ActiveExpense>Expense</ActiveExpense>
+            ) : (
+              <NoActive>Expense</NoActive>
+            )}
+          </TransactionToggleWrap>
 
-{isMinus && (
-<Select
-required
-options={transactionCategories}
-components={{ DropdownIndicator, IndicatorSeparator: () => null }}
-placeholder="Select a category"
-styles={selectStyle}
-isSearchable={false}
-// name="category"
-// {...register('value')}
-/>
-)}
+          {isMinus && (
+            <Select
+              required
+              options={transactionCategories}
+              components={{ DropdownIndicator, IndicatorSeparator: () => null }}
+              placeholder="Select a category"
+              styles={selectStyle}
+              isSearchable={false}
+              // name="category"
+              // {...register('value')}
+            />
+          )}
 
-<WrapSumCalendar>
-<Input
-type="number"
-name="amount"
-placeholder="0.00"
-{...register('amount')}
-/>
-<Calendar
-// {...register('date')}
-/>
-</WrapSumCalendar>
+          <WrapSumCalendar>
+            <Input
+              type="number"
+              name="amount"
+              placeholder="0.00"
+              {...register('amount')}
+            />
+            <Calendar
+            // {...register('date')}
+            />
+          </WrapSumCalendar>
 
-<TextareaStyled placeholder="Comment" {...register('comment')} />
+          <TextareaStyled placeholder="Comment" {...register('comment')} />
 
-<ButtonsWrap>
-<BtnAdd type="submit">Add</BtnAdd>
-<BtnCancel type="button" onClick={onCancelButton}>
-Cancel
-</BtnCancel>
-</ButtonsWrap>
-</Form>
-</Modal>
-</Backdrop>
-);
+          <ButtonsWrap>
+            <BtnAdd type="submit">Add</BtnAdd>
+            <BtnCancel type="button" onClick={onCancelButton}>
+              Cancel
+            </BtnCancel>
+          </ButtonsWrap>
+        </Form>
+      </Modal>
+    </Backdrop>
+  );
 };
--- src/components/AddTransactionModal/AddTransaction/AddTransaction.styled.jsx
+++ src/components/AddTransactionModal/AddTransaction/AddTransaction.styled.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { IoCloseOutline } from 'react-icons/io5';
 
 export const Backdrop = styled.div`
   position: fixed;
@@ -9,6 +10,7 @@ export const Backdrop = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color: rgba(34, 13, 91, 0.23);
   z-index: 10;
 
   &::before {
@@ -27,11 +29,17 @@ export const Modal = styled.div`
   flex-direction: column;
   align-items: center;
   max-width: 320px;
-  /* height: 100vh; */
   padding: 40px 20px;
+  background: rgba(255, 255, 255, 0.1);
   box-shadow: 0px 4px 60px 0px rgba(0, 0, 0, 0.25);
+  backdrop-filter: blur(50px);
   overflow: hidden;
-
+  @media only screen and (min-width: 768px) {
+    min-width: 540px;
+    min-height: 589px;
+    padding: 40px 73px;
+    border-radius: 8px;
+  }
   &::before {
     background-color: rgba(255, 255, 255, 0.1);
     content: '';
@@ -40,39 +48,26 @@ export const Modal = styled.div`
     inset: 0;
     z-index: -1;
   }
+`;
+
+export const Title = styled.h1`
+  margin-bottom: 34px;
+  color: #fbfbfb;
+  font-size: 24px;
+  font-weight: 400;
+  text-align: center;
 
   @media only screen and (min-width: 768px) {
-    width: 540px;
-    height: 589px;
-    padding: 40px 40px;
-    border-radius: 8px;
+    font-size: 30px;
+    margin-bottom: 42px;
   }
+`;
 
-  select:required {
-    color: rgba(255, 255, 255, 0.6);
-    padding-left: 8px;
-    padding-bottom: 8px;
-  }
-
-  h1 {
-    margin-bottom: 34px;
-    color: #fbfbfb;
-    font-size: 24px;
-    font-weight: 400;
-    text-align: center;
-
-    @media only screen and (min-width: 768px) {
-      font-size: 30px;
-      margin-bottom: 40px;
-    }
-  }
-  form {
-    display: flex;
-    flex-direction: column;
-    /* gap: 40px; */
-    margin-bottom: 20px;
-    min-width: 100%;
-  }
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+  width: 100%;
 `;
 
 export const CloseModalBtn = styled.button`
@@ -84,10 +79,8 @@ export const CloseModalBtn = styled.button`
   position: absolute;
   right: 20px;
   top: 20px;
-  /* background-color: transparent; */
   border: none;
   outline: transparent;
-  /* color: #ffffff; */
 
   @media only screen and (min-width: 768px) {
     display: flex;
@@ -102,14 +95,23 @@ export const TransactionToggleWrap = styled.div`
 `;
 
 export const Input = styled.input`
+  padding-left: 20px;
+  padding-bottom: 8px;
+  width: 100%;
   background: transparent;
   border: none;
   border-bottom: 1px solid rgba(255, 255, 255, 0.4);
   outline: none;
+  margin-bottom: 40px;
+  background: transparent;
+  color: #fbfbfb;
   font-size: 18px;
-  padding-left: 8px;
-  padding-bottom: 8px;
+  font-weight: 600;
+  @media only screen and (min-width: 768px) {
+    width: 181px;
+  }
 `;
+
 export const ButtonsWrap = styled.div`
   display: flex;
   align-items: center;
@@ -136,7 +138,7 @@ export const BtnAdd = styled.button`
   text-align: center;
   text-transform: uppercase;
   letter-spacing: 1.8px;
-  @media only screen and (mix-width: 768px) {
+  @media only screen and (min-width: 768px) {
     width: 300px;
   }
 `;
@@ -157,23 +159,25 @@ export const BtnCancel = styled.button`
   text-transform: uppercase;
   letter-spacing: 1.8px;
 
-  @media only screen and (mix-width: 768px) {
+  @media only screen and (min-width: 768px) {
     width: 300px;
   }
 `;
 
 export const TextareaStyled = styled.textarea`
-  height: 35px;
   margin-bottom: 40px;
+  padding-left: 20px;
+  padding-bottom: 52px;
   font-size: 18px;
   font-weight: 400;
   background-color: transparent;
   border: none;
   border-bottom: 1px solid rgba(255, 255, 255, 0.4);
   color: #ffffff;
-
-  &::placeholder {
-    /* margin-bottom: 52px; */
+  overflow: hidden;
+  outline: none;
+  @media only screen and (min-width: 768px) {
+    padding-bottom: 8px;
   }
 `;
 
@@ -196,23 +200,27 @@ export const ActiveIncome = styled.p`
 export const WrapSumCalendar = styled.div`
   display: flex;
   flex-direction: column;
-  margin-bottom: 40px;
   margin-top: 40px;
+  margin-bottom: 40px;
+  @media only screen and (min-width: 768px) {
+    flex-direction: row;
+    gap: 32px;
+    margin-top: 0px;
+  }
 
   input {
     background: transparent;
     border: none;
     border-bottom: 1px solid rgba(255, 255, 255, 0.4);
     outline: none;
-    padding: 8px 20px;
-    padding-left: 8px;
+    padding-left: 20px;
     padding-bottom: 8px;
     color: #fbfbfb;
     font-size: 18px;
     font-weight: 600;
     margin: 0;
   }
-  @media only screen and (mix-width: 768px) {
+  @media only screen and (min-width: 768px) {
     /* flex-direction: row; */
   }
 `;
@@ -230,7 +238,7 @@ export const InputToggle = styled.input`
   &:checked + ::before {
     transform: translateX(40px);
     background-color: #ff868d;
-    content: '-';
+    content: '';
   }
 `;
 
@@ -247,9 +255,9 @@ export const SpanToggle = styled.span`
 
   &::before {
     position: absolute;
-    content: '+';
-    left: -1px;
-    top: -1px;
+    content: '';
+    left: -2px;
+    top: -2px;
     width: 44px;
     height: 44px;
     border-radius: 44px;
@@ -259,6 +267,26 @@ export const SpanToggle = styled.span`
   }
 `;
 
+export const DateWrapper = styled.div`
+  margin-top: 40px;
+  position: relative;
+  display: flex;
+  /* align-items: center; */
+  input {
+    color: #fbfbfb;
+    font-size: 18px;
+    font-weight: 400;
+    width: 280px;
+    padding-left: 20px;
+
+    @media only screen and (min-width: 768px) {
+      width: 181px;
+    }
+  }
+`;
+
+export const CloseBtn = styled(IoCloseOutline)``;
+
 export const Gradient = styled.div`
   z-index: -20;
   position: absolute;
@@ -267,5 +295,5 @@ export const Gradient = styled.div`
   border-radius: 454px;
   background: rgba(47, 21, 176, 0.73);
   filter: blur(100px);
-  inset:0;
-  `
+  inset: 0;
+`;
