f736c79ccc5535c3ddd251a36fbfe67076c78035
SVTroya <my.svtroy@gmail.com>
2024-01-14 13:52:27 +0200

    Change balance

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import { persistor, store } from './reduxConfig/store';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
