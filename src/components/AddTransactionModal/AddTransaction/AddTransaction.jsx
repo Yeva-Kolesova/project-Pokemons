47781ffbf83f73b404c86d98d56995ef643938c4
MariiaZhk <maria8zhuk@gmail.com>
2024-01-14 22:12:58 +0100

    yup-btn-hovers

--- src/components/AddTransactionModal/AddTransaction/AddTransaction.jsx
+++ src/components/AddTransactionModal/AddTransaction/AddTransaction.jsx
@@ -5,7 +5,8 @@ import {
   ErrorMessage,
   Expense,
   Income,
-  InputErrorWrap, StyledSelect,
+  InputErrorWrap,
+  StyledSelect,
 } from './AddTransaction.styled';
 import {
   Backdrop,
@@ -53,16 +54,15 @@ const schema = yup
     amount: yup
       .number()
       .typeError('Please, enter the sum')
-      .min(1, 'Sum value must be at least 1 character')
       .required('Sum is required'),
     date: date().required('Date is required'),
     category: yup
       .string()
       .uuid('Incorrect format')
-      .when("isExpense", {
+      .when('isExpense', {
         is: true,
-        then: (schema) => schema.required('Category is required')
-      })
+        then: schema => schema.required('Category is required'),
+      }),
   })
   .required();
 
@@ -171,13 +171,13 @@ export const AddTransaction = ({ closeModal }) => {
       ...styles,
       color: '#FBFBFB',
       fontSize: '18px',
-      paddingLeft: isTabletOrDesktop ? '0' : '12px'
+      paddingLeft: isTabletOrDesktop ? '0' : '12px',
     }),
     placeholder: styles => ({
       ...styles,
       color: 'rgba(255, 255, 255, 0.6)',
       fontSize: '18px',
-      paddingLeft: isTabletOrDesktop ? '0' : '12px'
+      paddingLeft: isTabletOrDesktop ? '0' : '12px',
     }),
     menu: styles => ({
       ...styles,
